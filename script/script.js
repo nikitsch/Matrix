caa0d56c9d28f590c344ee428a689e553cc407e9
nikitsch <nikitsch5@gmail.com>
2023-04-08 16:26:05 +0300

    feat: main app logic

---
+++ script/script.js
@@ -0,0 +1,34 @@
+import { Column } from "./Column.js";
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const FONT_SIZE = 16;
+
+const columns = [];
+const columnsCount = canvas.width / FONT_SIZE;
+
+for (let i = 0; i < columnsCount; i++) {
+  if (i % 5) {
+    columns.push(new Column(i * FONT_SIZE - 3, FONT_SIZE + 5, canvas.height - 20, context));
+  } else {
+    continue;
+  }
+};
+
+context.font = `bold ${FONT_SIZE}px monospace`;
+
+function animate() {
+  context.fillStyle = "rgba(0, 0, 0, 0.06)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "green";
+  columns.forEach(column => column.darwSymbol());
+
+  setTimeout(() => requestAnimationFrame(animate), 50);
+}
+
+animate();
